480309a65514f499f5a8e050fa993c11e79b6f68
Sergio <sergioobolevich@gmail.com>
2020-11-12 12:25:12 -0300

    Add Devise Views, add post form, add devise controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,60 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  
-  def index
 
-  end
+  # before_action :authenticate_user!, only: %i[new create]
+
+
+
+  def index; end
+
+
 
   def new
+
     @post = Post.new
+
   end
+
+
 
   def create
 
+    @post = Post.new(posts_params)
+
+    if @post.save
+
+      redirect_to root_path
+
+    else
+
+      render :new
+
+    end
+
   end
 
-  def show
+
+
+  def show; end
+
+
+
+  def edit; end
+
+
+
+  def update; end
+
+
+
+  private
+
+
+
+  def posts_params
+
+    params.require(:post).permit(:title, :description)
 
   end
 
-  def edit
-
-  end
-
-  def update
-
-  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-
-  devise_for :users
-
   root 'posts#index'
-
   resources :posts, only: [:new, :create, :index]
-
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 end
-
